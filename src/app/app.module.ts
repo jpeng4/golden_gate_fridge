550f66e9b6f76cb3a03362b9498946604d2a8842
Julia Peng <julia.peng10@gmail.com>
2018-01-21 07:03:56 -0800

    fixed merged conflicts + barcode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,16 @@ import { HttpModule } from '@angular/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-<<<<<<< HEAD
+
 import { MyGroceriesPage } from '../pages/my-groceries/my-groceries'
 import { LoginPage } from '../pages/login/login';
 import { RegisterPage } from '../pages/register/register'
 import { AddItemPage} from '../pages/add-item/add-item'
-=======
-import { ListPage } from '../pages/list/list';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner';
->>>>>>> 211617cd285ea562bf4eca512dcc2d9cb47c028c
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { RestServiceProvider } from '../providers/rest-service/rest-service';
 
-import { HttpModule } from '@angular/http';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireModule } from 'angularfire2';
 import { FirebaseProvider } from '../providers/firebase/firebase';
@@ -47,14 +42,11 @@ const firebaseConfig = {
   imports: [
     BrowserModule,
     HttpModule,
-<<<<<<< HEAD
     AngularFireDatabaseModule,
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireAuthModule,
     IonicModule.forRoot(MyApp),
-=======
-    IonicModule.forRoot(MyApp)
->>>>>>> 211617cd285ea562bf4eca512dcc2d9cb47c028c
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -68,14 +60,9 @@ const firebaseConfig = {
   providers: [
     StatusBar,
     SplashScreen,
-<<<<<<< HEAD
     FirebaseProvider,
     BarcodeScanner,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-=======
-    BarcodeScanner,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
->>>>>>> 211617cd285ea562bf4eca512dcc2d9cb47c028c
   ]
 })
 export class AppModule {}
