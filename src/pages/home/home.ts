4df18212913c09a92ce8e8101d3e15d7f3a47680
kchennap <krishna.ceee@gmail.com>
2018-01-20 21:07:32 -0800

    First commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { BarcodeScanner ,BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  scanData : {};
+  encodeData : string ;
+  encodedData : {} ;
+  data = { };
+  option :BarcodeScannerOptions;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private barcodeScanner: BarcodeScanner) {
 
   }
 
+  scanner(){
+    console.log("Hello");
+    this.barcodeScanner.scan().then((barcodeData) => {
+      // Success! Barcode data is hered
+      console.log(barcodeData);
+      this.data = barcodeData;
+     }, (err) => {
+         // An error occurred
+         console.log(err);
+     });
+    //const results = await this.barcodeScanner.scan();
+    // console.log(results);
+  }    
 }
